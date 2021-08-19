6cc09259f4b0af7acc05e44768dc19310ff43c0b
kanatani-yuna <kpuuiy@hotmail.co.jp>
2021-08-19 20:21:54 +0900

    課題提出テスト　再提出
    JavaScript 1-4 の再提出

--- JavaStudy/JavaScript/1-4/1-4.js
+++ JavaStudy/JavaScript/1-4/1-4.js
@@ -1,16 +1,16 @@
 for (let i = 1; i <= 100; i++) {
 
+//3の倍数かつ5の倍数のとき、 FizzBuzz!! と出力します。(15の倍数、と考えるのはナシです。)
+if ( i % 3 == 0 && i % 5 == 0)
+    console.log("FizzBuzz!!");
+
 //3の倍数のとき、 Fizz! と出力します。
-if ( i % 3 == 0 ) 
+else if ( i % 3 == 0 ) 
     console.log("Fizz!");
  
 //5の倍数のとき、 Buzz! と出力します。
 else if ( i % 5 == 0 )
     console.log("Buzz!");
-
-//3の倍数かつ5の倍数のとき、 FizzBuzz!! と出力します。(15の倍数、と考えるのはナシです。)
-else if ( i % 3 == 0 && i % 5 == 0)
-    console.log("FizzBuzz!!");
 
 //3の倍数でも、5の倍数でもない場合は、その数を出力します。
 else
